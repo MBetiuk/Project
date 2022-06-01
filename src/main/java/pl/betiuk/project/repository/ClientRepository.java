783621c2e49f2f5d01999675da8f3b98ec14c026
MBetiuk <mbetiuk1@gmail.com>
2022-06-01 20:59:59 +0200

    Client update

--- src/main/java/pl/betiuk/project/repository/ClientRepository.java
+++ src/main/java/pl/betiuk/project/repository/ClientRepository.java
@@ -1,15 +1,17 @@
 package pl.betiuk.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.betiuk.project.model.Client;
 
-import java.awt.print.Book;
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends JpaRepository <Client, Long> {
 
     @Override
     List<Client> findAll();
+
+    Optional<Client> findById(Long id);
 }
