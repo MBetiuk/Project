20b7c4bff599d8daf29e0b4938ab9f0560c36cc7
MBetiuk <mbetiuk1@gmail.com>
2022-06-12 13:23:53 +0200

    question

---
+++ src/main/java/pl/betiuk/project/controller/StartController.java
@@ -0,0 +1,25 @@
+package pl.betiuk.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.betiuk.project.service.impl.DataService;
+
+@Controller
+@RequestMapping("/start")
+public class StartController {
+
+    private final DataService dataService;
+
+
+    public StartController(DataService dataService) {
+        this.dataService = dataService;
+    }
+
+    @GetMapping("")
+    public String start() {
+        dataService.loadData();
+        return "login";
+        // TODO: 12/06/2022 zrobić przekierowanie na strone logowania
+    }
+}
